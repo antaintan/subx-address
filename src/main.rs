193fa3db7f8ac9eef42d400d371ca74e885a082f
bitman <bitman@gmail.com>
2020-01-04 09:24:53 +0800

    优化代码生成地址，根据不同的网络

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
-#[macro_use] 
+#[macro_use]
 extern crate rocket_contrib;
 
 use chrono::Utc;
@@ -11,14 +11,17 @@ use std::collections::BTreeMap;
 use std::str::FromStr;
 use std::time::SystemTime;
 
-use substrate_keystore::Store;
+//use substrate_keystore::Store;
 
+use rocket::http::RawStr;
+//use rocket_contrib::json::{Json, JsonValue};
+use rocket_contrib::json::JsonValue;
 use substrate_primitives::crypto::Pair as CPair;
 use substrate_primitives::crypto::Ss58Codec;
 use substrate_primitives::ed25519::Pair;
-use tempdir::TempDir;
-use rocket_contrib::json::{Json, JsonValue};
-use rocket::http::RawStr;
+//use tempdir::TempDir;
+
+use substrate_primitives::hexdisplay::HexDisplay;
 
 fn main() {
     let mut map = BTreeMap::new();
@@ -26,7 +29,6 @@ fn main() {
     map.insert("DOT", 500);
     map.insert("BTC", 100);
 
-    println!("hello world!");
     println!("map: {:?}", map);
 
     // run job
@@ -37,13 +39,13 @@ fn main() {
         do_job();
     }
 
-    let temp_dir = TempDir::new("keystore").unwrap();
-    let store = Store::open(temp_dir.path().to_owned()).unwrap();
-    let key = store.generate("123123").unwrap();
+    // let temp_dir = TempDir::new("keystore").unwrap();
+    // let store = Store::open(temp_dir.path().to_owned()).unwrap();
+    // let key = store.generate("123123").unwrap();
 
     // launch web
     rocket::ignite()
-        .mount("/", routes![index, create])
+        .mount("/", routes![index, new_address])
         .launch();
 }
 
@@ -56,22 +58,16 @@ fn index() -> &'static str {
     "Hello, world!, rust web"
 }
 
-#[get("/address/create/<pwd>")]
-fn create(pwd: &RawStr) -> JsonValue {
-    let (pair, phrase) = Pair::generate_with_phrase(Some(pwd.as_str()));
+#[post("/address/newAddress?<pwd>")]
+fn new_address(pwd: &RawStr) -> JsonValue {
+    // 生成kusama网络地址, address prefix不同
+    let (pair, phrase, seed) = Pair::generate_with_phrase(Some(pwd.as_str()));
 
-    // 生成公钥, 私钥(seed)
-    println!("public_key = {:?}", pair.public());
-
-    let seed = "0x".to_string() + hex::encode(pair.seed()).as_str();
-    println!("seed = {}", seed);
-
-    // 生成地址
-    let address = pair.public().to_ss58check().to_string();
-    println!("address, {:?}", address);
-
-    // 生成助记词
-    println!("mnemonic = {:?}", phrase);
-
-    json!({ "code": 100, "seed": seed, "public": pair.public(), "address": address, "mnemonic": phrase })
+    json!({
+        "code": 100,
+        "mnemonic": phrase,
+        "secret seed": format!("0x{}", HexDisplay::from(&seed.as_ref())),
+        "public": format!("0x{}", HexDisplay::from(&pair.public().clone().as_ref())),
+        "SS58 address": pair.public().to_ss58check().to_string()
+    })
 }
